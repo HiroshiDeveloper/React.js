89aee3757065cd77fbacd15bde439bafa5402fe8
HiroshiDeveloper <19.hiroshi.13@gmail.com>
2016-11-04 20:14:50 -0700

    use react-bootstrap

--- TryOut/components/Input.jsx
+++ TryOut/components/Input.jsx
@@ -1,16 +1,32 @@
 import React, { Component, PropTypes } from "react"
-import DatePicker from 'react-bootstrap-date-picker';
+//import DatePicker from "../src/datepicker.jsx"
+import DatePicker from 'react-bootstrap-date-picker'
 import FormGroup from 'react-bootstrap/lib/FormGroup';
 import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+import FormControl from 'react-bootstrap/lib/FormControl';
 import HelpBlock from 'react-bootstrap/lib/HelpBlock';
 
 class Input extends Component {
+	
+	/*
 	getInitialState(){
+		console.log("TEST");
 		return {
 			value: new Date().toISOString()
 		}
     	}
-     	handleChange(value) {
+	*/
+
+	constructor(props) {
+		super(props);
+	     	this.state = {
+			value: new Date().toISOString()
+		};
+		this.handleChange = this.handleChange.bind(this);
+	}  
+
+     	handleChange(value){
+		console.log("PASS")
 		//value is an ISO String. 
 		this.setState({
 			value: value
@@ -20,9 +36,11 @@ class Input extends Component {
 		return(
 			<FormGroup>
 				<ControlLabel>DATE</ControlLabel>
-				<DatePicker value={this.props.value} onChange={this.handleChange} /><br/>
+				<DatePicker value={this.state.value} onChange={this.handleChange} /><br/>
 				<ControlLabel>TITLE</ControlLabel><br/>
-				<ControlLabel>DESCRIPTION</ControlLabel>
+				<FormControl type="text" /><br /><br/>
+				<ControlLabel>DESCRIPTION</ControlLabel><br/>
+				<FormControl type="text" />
 			</FormGroup>
 		)
 	}
